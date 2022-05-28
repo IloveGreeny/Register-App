7ac038e8aa2f05a5c013fef5f3f70070677cd6ac
IloveGreeny <greenxd23@gmail.com>
2022-05-28 15:36:16 +0400

    s

--- src/main.js
+++ src/main.js
@@ -69,8 +69,8 @@ app.post("/login", passport.authenticate("local", {
 app.use(checkAuthentication);
 
 app.get("/", (req, res) => {
-    res.redirect("https://ilovegreeny.github.io/web_create/")
-    //res.sendFile(path.resolve("public/home.html"));
+    //res.redirect("https://ilovegreeny.github.io/web_create/")
+    res.sendFile(path.resolve("public/home.html"));
 });
 
 app.get("/logout", (req, res) => {
